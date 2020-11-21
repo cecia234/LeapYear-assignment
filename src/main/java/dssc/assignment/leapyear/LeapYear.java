01af4a805ed360ab1d3e1312ab76e94b8037572f
cecia234 <cecia234@gmail.com>
2020-11-21 12:50:47 +0100

    removed duplication

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -5,16 +5,5 @@ public class LeapYear {
     public Boolean n_divisible_by_m(int n, int m){
         return n % m == 0;
     }
-    public Boolean divisibleBy4(int n){
-        return n % 4 == 0;
-    }
-
-    public Boolean divisibleBy100(int n){
-        return n%100 == 0;
-    }
-
-    public Boolean divisibleBy400(int n){
-        return n%400 == 0;
-    }
-
+   
 }
--- src/test/java/dssc/assignment/leapyear/NumberDivisibility.java
+++ src/test/java/dssc/assignment/leapyear/NumberDivisibility.java
@@ -16,28 +16,6 @@ public class NumberDivisibility {
 
     }
 
-   @ParameterizedTest
-   @CsvSource({"2000, true","1996, true","2001, false" ,"1900, true"})
-   void numbersDivisible_4(int number , Boolean expected){
-       LeapYear leapYear = new LeapYear();
-       assertEquals(expected,leapYear.divisibleBy4(number));
-
-   }
-
-    @ParameterizedTest
-    @CsvSource({"2000, true","1996, false","2001, false" ,"1900, true"})
-    void numbersDivisible_100(int number , Boolean expected){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(expected,leapYear.divisibleBy100(number));
-    }
-
-
-    @ParameterizedTest
-    @CsvSource({"2000, true","1996, false","2001, false" ,"1900, false"})
-    void numbersDivisible_400(int number , Boolean expected){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(expected,leapYear.divisibleBy400(number));
-    }
 
 
 
