6dbb7aac923443ab5ba3314c35545a77e9ead672
cecia234 <cecia234@gmail.com>
2020-11-21 12:49:26 +0100

    Added method for checking divisibility n/m

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -2,6 +2,9 @@ package dssc.assignment.leapyear;
 
 public class LeapYear {
 
+    public Boolean n_divisible_by_m(int n, int m){
+        return n % m == 0;
+    }
     public Boolean divisibleBy4(int n){
         return n % 4 == 0;
     }
