404ce514fa8eae1a65f4b71dc4a0fff37dee6ed9
cecia234 <cecia234@gmail.com>
2020-11-21 13:18:42 +0100

    Added method to check if a year is Leap

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -5,5 +5,12 @@ public class LeapYear {
     public Boolean n_divisible_by_m(int n, int m){
         return n % m == 0;
     }
-   
+
+    public Boolean isLeapYear(int n){
+        if(!n_divisible_by_m(n,400))
+            return !n_divisible_by_m(n, 100) && n_divisible_by_m(n, 4);
+        else return true;
+
+    }
+
 }
