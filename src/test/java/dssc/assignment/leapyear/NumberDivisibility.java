6b38d9659412a2cb529b31e729a71d9748b5cf26
cecia234 <cecia234@gmail.com>
2020-11-21 12:27:03 +0100

    added ParametrizedTest

--- src/test/java/dssc/assignment/leapyear/NumberDivisibility.java
+++ src/test/java/dssc/assignment/leapyear/NumberDivisibility.java
@@ -1,10 +1,20 @@
 package dssc.assignment.leapyear;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberDivisibility {
+
+   @ParameterizedTest
+   @CsvSource({"2000, true","1996, true","2001, false" ,"1900, true"})
+   void numbersDivisible_4(int number , Boolean expected){
+       LeapYear leapYear = new LeapYear();
+       assertEquals(expected,leapYear.divisibleBy4(number));
+
+   }
 
     @Test
    void twoThousands_divisible_by_4(){
@@ -39,7 +49,7 @@ public class NumberDivisibility {
     @Test
     void nineteen00_divisible_by_400(){
         LeapYear leapYear = new LeapYear();
-        assertEquals(false,leapYear.divisibleBy400(2000));
+        assertEquals(false,leapYear.divisibleBy400(1900));
     }
 
 
