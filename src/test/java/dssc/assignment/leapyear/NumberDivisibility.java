00fe3e6625e5b50d2f5eec4627f609dc2aa66c32
cecia234 <cecia234@gmail.com>
2020-11-21 12:30:03 +0100

    removed duplication in number divisibility by 4

--- src/test/java/dssc/assignment/leapyear/NumberDivisibility.java
+++ src/test/java/dssc/assignment/leapyear/NumberDivisibility.java
@@ -16,43 +16,4 @@ public class NumberDivisibility {
 
    }
 
-    @Test
-   void twoThousands_divisible_by_4(){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(true,leapYear.divisibleBy4(2000));
-    }
-
-    @Test
-    void twoThousands01_divisible_by_4(){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(false,leapYear.divisibleBy4(2001));
-    }
-
-    @Test
-    void twoThousands_divisible_by_100(){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(true,leapYear.divisibleBy100(2000));
-    }
-
-    @Test
-    void twoThousands_divisible_by_400(){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(true,leapYear.divisibleBy400(2000));
-    }
-
-    @Test
-    void nineteen00_divisible_by_100(){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(true,leapYear.divisibleBy100(2000));
-    }
-
-    @Test
-    void nineteen00_divisible_by_400(){
-        LeapYear leapYear = new LeapYear();
-        assertEquals(false,leapYear.divisibleBy400(1900));
-    }
-
-
-
-
 }
